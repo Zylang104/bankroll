a3dfb7b5a0c9bac4c67841478f2f30872e8b316e
Zylang <zylang@proton.me>
2026-04-04 01:11:16 +0200

    Create premium.lua

---
+++ premium.lua
@@ -0,0 +1,2 @@
+-- for key join server right now it's on beta
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/a332406a442bd80ce2f611eae769460477887f53ce98d10a67456d18bb49e02c/download"))()
