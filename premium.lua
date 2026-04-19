f2b5d0278dfbc17090501479a896913398edb3ef
Zylang <joninurka7@gmail.com>
2026-04-19 02:08:35 +0200

    Update premium.lua

--- premium.lua
+++ premium.lua
@@ -1,2 +1,1 @@
--- for key join server right now it's on beta
-loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/a332406a442bd80ce2f611eae769460477887f53ce98d10a67456d18bb49e02c/download"))()
+print("premium is now down") 
